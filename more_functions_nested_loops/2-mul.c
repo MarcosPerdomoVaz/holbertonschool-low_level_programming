b4a4870324f3b51fa202099f6321150aa7ce3c0d
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-02-22 09:03:13 -0800

    new code

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- *  * _isdigit -  a function that checks for a digit 0-9
- *   * @c: input
- *    * Return: 1 if c is a digit, 0 otherwise
- *     **/
-int _isdigit(int c)
+ *  *  mul - a function that multiplies two integers
+ *   * @a: first input
+ *    * @b: second input
+ *     * Return: multiplication result of the 2 inputs
+ *      **/
+int mul(int a, int b)
 {
-	if ((c >= 48) && (c <= 57))
-	return (1);
-	else
-	return (0);
+	return (a * b);
 }
