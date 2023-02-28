04001b38efe7e1a982938e823bf808515b4783ba
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-02-28 08:40:03 -0800

    new code

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *  * _strcpy - function that copies a string
- *   * @dest: destination to copy to
- *    * @src: string to be copied
- *     * Return: 0
+ ** _strcpy - function that copies a string
+ ** @dest: destination to copy to
+ ** @src: string to be copied
+ ** Return: 0
  *     **/
 char *_strcpy(char *dest, char *src)
 {
-	int a;
-
-	for (a = 0; src[a] != '\0'; a++)
-{
-	dest[a] = src[a];
-}
+	sprintf(dest, "%s", src);
 	return (dest);
 }
