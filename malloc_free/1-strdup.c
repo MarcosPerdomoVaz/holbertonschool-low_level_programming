514480b293c6f881d3541a6eba9d7ef2b2938fc9
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-03-14 14:11:10 -0700

    new code

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,30 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * _strdup - duplicates a string into newly allocated array
+ *
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string
  */
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	int size = 0;
+	char *ptr, *ret;
 
-	if (str == NULL)
+	if (!str)
 	return (NULL);
 
-	for (end = 0; end <= *str; end++)
-{
-}
+	ptr = str;
+	while (*ptr++)
+	size++;
 
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-	array[i] = str[i];
-
-	if (array == NULL)
+	ret = malloc(size + 1);
+	if (!ret)
 	return (NULL);
-	return (array);
+
+	ptr = ret;
+	while (*str)
+	*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
 }
